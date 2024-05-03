cb2bf15363666e2e7374fbda9d41287afc217707
Christelle CHARLES-ALFRED <christellecharlesalfred56@gmail.com>
2024-05-04 01:12:54 +0200

    mise en place de l'environement frontend react

---
+++ frontend/argentBankReact/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router-dom";
+
+function Header() {
+    return (
+        <nav className="main-nav">
+            <NavLink className="main-nav-logo" to="/">
+                <img
+                    className="main-nav-logo-image"
+                    src="src/assets/img/argentBankLogo.png"
+                    alt="Argent Bank Logo"
+                />
+                <h1 className="sr-only">Argent Bank</h1>
+            </NavLink>
+            <div>
+                <NavLink className="main-nav-item" to="/SignIn">
+                    <i className="fa fa-user-circle"></i>
+                    Sign In
+                </NavLink>
+            </div>
+        </nav>
+    );
+}
+
+export default Header;
